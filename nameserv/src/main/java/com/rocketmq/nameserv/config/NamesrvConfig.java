f5948d2b489a61471910ad5a463984046dd5917f
zengqz <954723922@qq.com>
2022-12-07 22:53:36 +0800

    定义NamesrvConfig类

--- nameserv/src/main/java/com/rocketmq/nameserv/config/NamesrvConfig.java
+++ nameserv/src/main/java/com/rocketmq/nameserv/config/NamesrvConfig.java
@@ -7,4 +7,59 @@ package com.rocketmq.nameserv.config;
  * @Version: 1.0
  */
 public class NamesrvConfig {
+
+    private String rocketmqHome;
+    private String kvConfigPath;
+    private String configStorePath;
+    private String productEnvName = "center";
+    private boolean clusterTest = false;
+    private boolean orderMessageEnable = false;
+
+    public String getRocketmqHome() {
+        return rocketmqHome;
+    }
+
+    public void setRocketmqHome(String rocketmqHome) {
+        this.rocketmqHome = rocketmqHome;
+    }
+
+    public String getKvConfigPath() {
+        return kvConfigPath;
+    }
+
+    public void setKvConfigPath(String kvConfigPath) {
+        this.kvConfigPath = kvConfigPath;
+    }
+
+    public String getConfigStorePath() {
+        return configStorePath;
+    }
+
+    public void setConfigStorePath(String configStorePath) {
+        this.configStorePath = configStorePath;
+    }
+
+    public String getProductEnvName() {
+        return productEnvName;
+    }
+
+    public void setProductEnvName(String productEnvName) {
+        this.productEnvName = productEnvName;
+    }
+
+    public boolean isClusterTest() {
+        return clusterTest;
+    }
+
+    public void setClusterTest(boolean clusterTest) {
+        this.clusterTest = clusterTest;
+    }
+
+    public boolean isOrderMessageEnable() {
+        return orderMessageEnable;
+    }
+
+    public void setOrderMessageEnable(boolean orderMessageEnable) {
+        this.orderMessageEnable = orderMessageEnable;
+    }
 }
