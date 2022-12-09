cd08702565db97c58bf54f11e28711659b64d276
zengqz <954723922@qq.com>
2022-12-09 22:41:01 +0800

    定义NettyServerConfig类

--- nameserv/src/main/java/com/rocketmq/nameserv/config/NettyServerConfig.java
+++ nameserv/src/main/java/com/rocketmq/nameserv/config/NettyServerConfig.java
@@ -7,4 +7,15 @@ package com.rocketmq.nameserv.config;
  * @Version: 1.0
  */
 public class NettyServerConfig implements Cloneable {
+
+    private int listenPort = 8888;
+    private int serverWorkerThreads = 8;
+    private int serverCallbackExecutorThreads = 0;
+    private int serverSelectorThreads = 3;
+    private int serverOnewaySemphoreValue = 256;
+    private int serverAsyncSemaphoreValue = 64;
+    private int serverChannelMaxIdleTimeSeconds = 120;
+
+
+
 }
